2ddfe895c19da1a2fd31e314292e98e4da02b812
Nirav Surabhi <vinaysurabhi@gmail.com>
2019-10-19 14:37:12 -0500

    Crane Mission Changes - Going from launch to crane.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import logging # this is used for logging
 logging.info("Beginning of the Runs.")
 
 #uncomment one of the functions below to run your missions
+#New Comment her
 #Go_To_Crane()
 Go_To_Tree()
 #Go_To_Swing()
