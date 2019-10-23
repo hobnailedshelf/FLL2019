b38ae9266431149fd68a265711873b15d768f09b
Nirav Surabhi <vinaysurabhi@gmail.com>
2019-10-22 20:24:57 -0500

    Tree - Rohan - Rubber band change

--- Run_Two.py
+++ Run_Two.py
@@ -3,25 +3,14 @@ from Initialize import *
 from Common_Functions import * # import all the main functions
 import logging # this is used for logging
 
-#def Go_To_Tree():   
-#   logging.info("Starting of the tree mission.")
-#  go_straight(300,-1,"l",1,"l",35,"l","b")
-# logging.info("reached black line.")
-#    line_follower(100,-1,"l",1,"d",720,"X","X")
-#    go_straight(300,-1,"r",1,"d",1200,"x","x")
-#    go_straight(50,-1,"r",1,"d",400,"x","x")
-#    wait(5000)
-    #go_straight(1000,-1,"r",-1,"d",2500,"x","x")
-    #go_straight(300,-1,"r",1,"l",5,"r","w")
-    #wait(5)
-    #go_straight(50,-1,"r",1,"d",150,"l","w")
-    #wait(5)
-    #go_straight(300,1,"r",-1,"d",3000,"l","w")
-
-# Testing with Medium Motor
-
-def Go_To_Tree():   
+def Go_To_Tree():
     logging.info("Starting of the tree mission.")
-    go_straight(300,-1,"l",1,"l",35,"l","b")
-    Med_Motor_1.run_time(90,1400)
-    
+    go_straight(2000,1,"l",-1,"d",800,"l","b")
+    Left_Motor.run_angle(200,278)
+    go_straight(2000,1,"l",-1,"d",3200,"l","b")
+    wait(500)
+    Med_Motor_1.run_time(110,1000)
+    Robot.drive_time(1000, 0, 4000)
+    wait(6000)
+    go_straight(2000,1,"l",-1,"d",1900,"l","b")
+    go_straight(2000,-1,"l",1,"d",1900,"l","b")
